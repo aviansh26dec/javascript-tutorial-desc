2216ed9352336acc5204a616e406029e2ccbb700
Avinash Singh <avinashkumarsingh.ignou@gmail.com>
2024-08-09 16:02:55 +0530

    calendar

---
+++ Javascript Basic/004-Date.js
@@ -0,0 +1,12 @@
+let newDate = new Date();
+
+// console.log(newDate);
+// console.log(newDate.toString());
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth());
+// console.log(newDate.getFullYear());
+// console.log(newDate.toLocaleString());
+// console.log(newDate.toLocaleString('en-IN', { timeZone: 'Europe/London' }));
+
+// let firstDay = newDate.getDay();
+
---
+++ Javascript Basic/005-Calendar.js
@@ -0,0 +1,9 @@
+let newDate = new Date();
+
+let month = newDate.getMonth();
+let year = newDate.getFullYear();
+let lastDate = new Date(newDate.getFullYear(), newDate.getMonth() + 1, 0).getDate();
+
+for(let i = 0; i <= lastDate; i++ ){
+    
+}
